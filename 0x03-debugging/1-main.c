e195f042a773fbb810a68e0a53c0ca74a57aea9a
RachaelYusuf <rachaelroy11@gmail.com>
2023-04-25 17:08:01 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 
 	 i = 0;
@@ -16,9 +16,7 @@ int main(void)
 	/*{*/
 	/*	putchar(i);*/
 	/*}*/
-	
 	printf("Infinite loop avoided! \\o/\n");
-	
 	return (0);
 }
 
